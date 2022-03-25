3af2ce69ae7e0579a765e0e21ef368b88a4502cd
yuichiro-tanaka <yuichiro.tanaka.75@gmail.com>
2022-03-25 17:10:52 +0900

    up

--- fpga-examples/paddle_inputs.v
+++ fpga-examples/paddle_inputs.v
@@ -12,42 +12,44 @@ module paddles_top
     wire [15:0] hpos;
     wire [15:0] vpos;
 
-    reg [7:0] player_x;
-    reg [7:0] player_y;
+    // player position (only set at VSYNC)
+    reg [15:0] player_x;
+    reg [15:0] player_y;
 
-    reg [7:0] paddle_x;
-    reg [7:0] paddle_y;
+    // paddle position (set continuously during frame)
+    reg [15:0] paddle_x;
+    reg [15:0] paddle_y;
 
+    // read horizontal paddle
     always @(posedge hpaddle)
-        paddle_x <= vpos[7:0];
+        paddle_x <= vpos[15:0];
 
+    // read vertical paddle
     always @(posedge vpaddle)
-        paddle_y <= vpos[7:0];
+        paddle_y <= vpos[15:0];
 
+    // update player_x and player_y
     always @(posedge vsync)
-        begin
-            player_x <= paddle_x;
-            player_y <= paddle_y;
-        end
+    begin
+        player_x <= paddle_x;
+        player_y <= paddle_y;
+    end
 
     hvsync_generator hvsync_gen(
-        .clk(clk),
-        .reset(reset),
-        .hsync(hsync),
-        .vsync(vsync),
-        .display_on(display_on),
-        .hpos(hpos),
-        .vpos(vpos)
+    .clk(clk),
+    .reset(reset),
+    .hsync(hsync),
+    .vsync(vsync),
+    .display_on(display_on),
+    .hpos(hpos),
+    .vpos(vpos)
     );
 
+    // display paddle positions on screen
 
-    wire h = hpos[7:0] >= paddle_x;
-    wire v = vpos[7:0] >= paddle_y;
+    wire h = hpos[15:0] >= paddle_x;
+    wire v = vpos[15:0] >= paddle_y;
 
-    assign rgb = {
-        1'b0,
-        display_on && h, 
-        display_on && v
-    };
+    assign rgb = {1'b0, display_on && h, display_on && v};
 
 endmodule
